b5a9482600cbb219c70a885b4ada6ac4ea15317c
annatvali <anntvaliashvili@gmail.com>
2024-03-18 17:40:25 +0400

    chore: add data.js to have navigation info in one object

---
+++ 1-components/src/data/nav.js
@@ -0,0 +1,22 @@
+export const nav = [
+  {
+    id: 1,
+    name: 'Home',
+    url: '/',
+  },
+  {
+    id: 2,
+    name: 'About',
+    url: '/about',
+  },
+  {
+    id: 3,
+    name: 'Services',
+    url: '/services',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    url: '/contact',
+  },
+];
